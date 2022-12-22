207b79a30430a86e8d448908fe5422e15536986f
Maxim Karachyov <karachyov.m@gmail.com>
2022-12-22 14:17:47 +0300

    unilook-video

--- src/js/galamart-unilook.js
+++ src/js/galamart-unilook.js
@@ -2,32 +2,15 @@
 
 
 
-document.querySelectorAll('.newyear2023-block').forEach(n => {
-    const swiper = new Swiper(n.querySelector('.swiper-block-item'), {
-        slidesPerView: 5,
-        spaceBetween: 30,
+document.querySelectorAll('.unilook-video').forEach(n => {
+    const swiper = new Swiper(n.querySelector('.swiper-video'), {
+        slidesPerView: 1,
+        spaceBetween: 0,
         observer: true,
         observeParents: true,
         navigation: {
             nextEl: n.querySelector('.swiper-button-next'),
             prevEl: n.querySelector('.swiper-button-prev'),
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-            480: {
-                slidesPerView: 3,
-            },
-            768: {
-                slidesPerView: 4,
-                spaceBetween: 30,
-            },
-            1024: {
-                slidesPerView: 5,
-                spaceBetween: 30,
-            }
         }
     })
 });

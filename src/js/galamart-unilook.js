dd394dc16ae99d6ed130c46ad278f35118deb22a
Maxim Karachyov <karachyov.m@gmail.com>
2022-12-25 21:07:08 +0300

    fix modal

--- src/js/galamart-unilook.js
+++ src/js/galamart-unilook.js
@@ -1,4 +1,6 @@
 @@include('./partials/swiper-bundle.min.js')
+@@include('./partials/remodal.js')
+@@include('./partials/jquery.fancybox.min.js')
 
 
 
@@ -6,15 +8,27 @@ document.querySelectorAll('.unilook-idea').forEach(n => {
     const slider = n.querySelector('.swiper-idea');
     const type = slider.getAttribute('data-type');
     const swiper = new Swiper(slider, {
-        slidesPerView: 1,
-        spaceBetween: 0,
+        slidesPerView: type === 'modal'? 2 : 1,
+        spaceBetween: type === 'modal'? 15 : 0,
         initialSlide: type === 'video'? 0 : 1,
-        centeredSlides: true,
+        centeredSlides: type === 'modal'? false : true,
         observer: true,
         observeParents: true,
         navigation: {
             nextEl: n.querySelector('.swiper-button-next'),
             prevEl: n.querySelector('.swiper-button-prev'),
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: type === 'modal'? 1.2 : 1,
+                spaceBetween: 0,
+                initialSlide: 0,
+            },
+            768: {
+                slidesPerView: type === 'modal'? 2 : 1,
+                spaceBetween: type === 'modal'? 15 : 0,
+                initialSlide: type === 'video'? 0 : 1,
+            }
         }
     })
 });

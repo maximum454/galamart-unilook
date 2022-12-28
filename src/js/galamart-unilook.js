3380cd13aa3064d68b02202f727f7a6541089be9
Maxim Karachyov <karachyov.m@gmail.com>
2022-12-28 15:35:42 +0300

    fix

--- src/js/galamart-unilook.js
+++ src/js/galamart-unilook.js
@@ -2,11 +2,17 @@
 @@include('./partials/remodal.js')
 @@include('./partials/jquery.fancybox.min.js')
 
-
-
 document.querySelectorAll('.unilook-idea').forEach(n => {
     const slider = n.querySelector('.swiper-idea');
     const type = slider.getAttribute('data-type');
+    const arrowNext = n.querySelector('.swiper-button-next')
+    const arrowPrev = n.querySelector('.swiper-button-prev')
+    const content = slider.querySelector('.unilook-idea__content');
+    if(content){
+        arrowNext.style.top = `calc(50% - ${content.clientHeight/2}px)`;
+        arrowPrev.style.top = `calc(50% - ${content.clientHeight/2}px)`;
+    }
+
     const swiper = new Swiper(slider, {
         slidesPerView: type === 'modal'? 2 : 1,
         spaceBetween: type === 'modal'? 15 : 0,
@@ -15,8 +21,8 @@ document.querySelectorAll('.unilook-idea').forEach(n => {
         observer: true,
         observeParents: true,
         navigation: {
-            nextEl: n.querySelector('.swiper-button-next'),
-            prevEl: n.querySelector('.swiper-button-prev'),
+            nextEl: arrowNext,
+            prevEl: arrowPrev,
         },
         breakpoints: {
             320: {
@@ -31,4 +37,15 @@ document.querySelectorAll('.unilook-idea').forEach(n => {
             }
         }
     })
+
+
+});
+
+
+$('.fancybox-unilook').fancybox({
+    arrows: false,
+    infobar: false,
+    buttons: [
+        "close"
+    ],
 });
